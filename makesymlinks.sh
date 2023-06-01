00c6c07a3e30970531b3246fb4b4fda23bdafc87
Markus Eckle <markus.eckle@diehl.com>
2023-06-01 12:57:01 +0200

    add zsh install if needed to script

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -29,3 +29,40 @@ for file in $files; do
     echo "Creating symlink to $file in home directory."
     ln -s $dir/$file ~/.$file
 done
+
+install_zsh () {
+# Test to see if zshell is installed.  If it is:
+if [ -f /bin/zsh -o -f /usr/bin/zsh ]; then
+    # Clone my oh-my-zsh repository from GitHub only if it isn't already present
+    if [[ ! -d ~/.oh-my-zsh/ ]]; then
+	git clone https://github.com/ohmyzsh/ohmyzsh.git ~
+    fi
+    # Clone zsh-autosuggestion and zsh-syntaxhighlighting if not present
+    if [[ ! -d ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions ]]
+	git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions
+    fi
+    if [[ ! -d ~/.oh-my-zsh/custom/plugins/zsh-zsh-syntax-highlighting ]]
+	git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
+    fi
+    # Set the default shell to zsh if it isn't currently set to zsh
+    if [[ ! $(echo $SHELL) == $(which zsh) ]]; then
+        chsh -s $(which zsh)
+    fi
+else
+    # If zsh isn't installed, get the platform of the current machine
+    platform=$(uname);
+    # If the platform is Linux, try an apt-get to install zsh and then recurse
+    if [[ $platform == 'Linux' ]]; then
+        if [[ -f /etc/debian_version ]]; then
+	    echo "zsh isn't install, installing via apt-get"
+            sudo apt-get install zsh
+            install_zsh
+	    echo " ... done"
+	else
+	    echo "zsh isn't installed, install zsh and rerun script"
+	    exit
+        fi
+fi
+}
+
+install_zsh
